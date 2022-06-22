d7377b32604802b4c90f5a29fa43518cd4c9fdf0
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2022-06-22 18:26:18 +0900

    2022/06/22

---
+++ 문자열 내 마음대로 정렬하기(다른풀이).js
@@ -0,0 +1,21 @@
+// case1
+function solution(strings, n) {
+  return strings.sort((a, b) => {
+    if (a[n] > b[n]) return 1;
+    if (a[n] < b[n]) return -1;
+
+    if (a[n] == b[n]) {
+      if (a > b) return 1;
+      if (a < b) return -1;
+    }
+  });
+}
+console.log(solution(["sun", "bed", "car"], 1));
+
+// case2
+// function solution(strings, n) {
+
+//     return strings.sort((a,b)=> a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]))
+
+// }
+// console.log(solution(["sun", "bed", "car"], 1));
